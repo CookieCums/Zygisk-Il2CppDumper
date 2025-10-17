55f85056188008596028e9acd2777b6747cd4964
Gautam <159130133+CookieCums@users.noreply.github.com>
2025-10-18 02:06:20 +0530

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.indusgame.play"
+#define GamePackageName "com.criticalforceentertainment.criticalops"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
